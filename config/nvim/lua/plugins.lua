fdc487cf30f2109ab5171256fae6a57c957237f0
mike <imincairo@gmail.com>
2022-04-20 18:33:07 +0700

    plugins.lua updated. tab completion enabled. lua and golang support

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,110 +1,163 @@
-return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
+require('packer').startup(function(use)
+  --packer
+  use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
+  --treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
 
-	use 'neovim/nvim-lspconfig'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-cmdline'
-	use 'hrsh7th/nvim-cmp'
+  --lsp
+  use 'neovim/nvim-lspconfig'
 
-	use 'hrsh7th/cmp-vsnip'
-	use 'hrsh7th/vim-vsnip'
-
-	local cmp = require'cmp'
-	  cmp.setup({
-	    snippet = {
-	      -- REQUIRED - you must specify a snippet engine
-	      expand = function(args)
-		vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-	      end,
-	    },
-	    window = {
-	      -- completion = cmp.config.window.bordered(),
-	      -- documentation = cmp.config.window.bordered(),
-	    },
-	    mapping = cmp.mapping.preset.insert({
-	      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-	      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-	      ['<C-Space>'] = cmp.mapping.complete(),
-	      ['<C-e>'] = cmp.mapping.abort(),
-	      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	    }),
-	    sources = cmp.config.sources({
-	      { name = 'nvim_lsp' },
-	      { name = 'vsnip' }, -- For vsnip users.
-	      -- { name = 'luasnip' }, -- For luasnip users.
-	      -- { name = 'ultisnips' }, -- For ultisnips users.
-	      -- { name = 'snippy' }, -- For snippy users.
-	    }, {
-	      { name = 'buffer' },
-	    })
-	  })
-
-	  -- Set configuration for specific filetype.
-	  cmp.setup.filetype('gitcommit', {
-	    sources = cmp.config.sources({
-	      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-	    }, {
-	      { name = 'buffer' },
-	    })
-	  })
-
-	  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-	  cmp.setup.cmdline('/', {
-	    mapping = cmp.mapping.preset.cmdline(),
-	    sources = {
-	      { name = 'buffer' }
-	    }
-	  })
-
-	  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-	  cmp.setup.cmdline(':', {
-	    mapping = cmp.mapping.preset.cmdline(),
-	    sources = cmp.config.sources({
-	      { name = 'path' }
-	    }, {
-	      { name = 'cmdline' }
-	    })
-	  })
-
-	  -- Setup lspconfig.
-	  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-	  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-	  require('lspconfig')['pyright'].setup {
-	    capabilities = capabilities
-	  }
-	local runtime_path = vim.split(package.path, ';')
-	table.insert(runtime_path, "lua/?.lua")
-	table.insert(runtime_path, "lua/?/init.lua")
-
-	require'lspconfig'.sumneko_lua.setup {
-	  settings = {
-	    Lua = {
-	      runtime = {
-		version = 'LuaJIT',
-		-- Setup your lua path
-		path = runtime_path,
-	      },
-	      diagnostics = {
-		-- Get the language server to recognize the `vim` global
-		globals = {'vim'},
-	      },
-	      workspace = {
-		-- Make the server aware of Neovim runtime files
-		library = vim.api.nvim_get_runtime_file("", true),
-	      },
-	      telemetry = {
-		enable = false,
-	      },
-	    },
-	  },
-	}
-
-
+  --auto complete
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  --snippet
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+  vim.opt.completeopt = { "menu", "menuone", "noselect" }
 end)
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+
+local cmp = require('cmp')
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    end,
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'cmp_git' },
+    }, {
+      { name = 'buffer' },
+    })
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work)
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+})
+
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()) --nvim-cmp
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
+
+-- Setup lspconfig.
+local nvim_lsp = require('lspconfig')
+
+-- setup languages
+-- GoLang
+nvim_lsp['gopls'].setup{
+  cmd = {'gopls'},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      experimentalPostfixCompletions = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+  init_options = {
+    usePlaceholders = true,
+  }
+}
+
+-- lua
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+nvim_lsp['sumneko_lua'].setup {
+	cmd = {'lua-language-server'},
+	on_attach = on_attach,
+	capabilities = capabilities,
+  	settings = {
+    		Lua = {
+      			runtime = {
+	version = 'LuaJIT',
+	-- Setup your lua path
+	path = runtime_path,
+      },
+      diagnostics = {
+	-- Get the language server to recognize the `vim` global
+	globals = {'vim'},
+      },
+      workspace = {
+	-- Make the server aware of Neovim runtime files
+	library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+	enable = false,
+      },
+    },
+  },
+}
